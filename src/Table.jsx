9cbeb3442dd51c1d86f26e5245e6d2b47c9b6dd8
Caprice Yuri <123914679+CapriceYuri@users.noreply.github.com>
2024-01-16 12:08:51 -0500

    Testing git

--- src/Table.jsx
+++ src/Table.jsx
@@ -92,19 +92,11 @@ export default function Table() {
         <div className="mb-8 flex items-center justify-between gap-8">
           <div>
             <Typography variant="h5" color="blue-gray">
-              Members list
+              Players list
             </Typography>
             <Typography color="gray" className="mt-1 font-normal">
-              See information about all members
+              See information about all players
             </Typography>
-          </div>
-          <div className="flex shrink-0 flex-col gap-2 sm:flex-row">
-            <Button variant="outlined" size="sm">
-              view all
-            </Button>
-            <Button className="flex items-center gap-3" size="sm">
-              <UserPlusIcon strokeWidth={2} className="h-4 w-4" /> Add member
-            </Button>
           </div>
         </div>
         <div className="flex flex-col items-center justify-between gap-4 md:flex-row">
@@ -117,12 +109,6 @@ export default function Table() {
               ))}
             </TabsHeader>
           </Tabs>
-          <div className="w-full md:w-72">
-            <Input
-              label="Search"
-              icon={<MagnifyingGlassIcon className="h-5 w-5" />}
-            />
-          </div>
         </div>
       </CardHeader>
       <CardBody className="overflow-scroll px-0">
