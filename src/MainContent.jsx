9f13459704f4381229fc0bfcbcf78b48cf8a68e2
Caprice Yuri <123914679+CapriceYuri@users.noreply.github.com>
2024-01-19 10:03:15 -0500

    Git

--- src/MainContent.jsx
+++ src/MainContent.jsx
@@ -49,7 +49,7 @@ export default function MainSection() {
   // Functions & Callbacks
 
   return (
-    <section className="grid grid-cols-2 xl:grid-cols-4 relative h-lvh">
+    <section className="grid grid-cols-2 xl:grid-cols-5 relative h-lvh">
       <img
         src="bgtesting.png"
         className="fixed h-lvh w-full object-cover -z-10"
@@ -57,7 +57,7 @@ export default function MainSection() {
 
       <section className="grid grid-cols-1 relative order-2 xl:order-1 mx-auto w-full h-full">
         <Card
-          className="relative lg:sticky lg:top-0 h-[80%] bg-transparent"
+          className="relative lg:sticky lg:top-0 h-full xl:h-1/2 bg-transparent"
           shadow={false}
         >
           <List className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-1 mx-auto">
@@ -99,7 +99,7 @@ export default function MainSection() {
         </Card>
       </section>
 
-      <section className="py-5 col-span-2 order-1 xl:order-2 h-full">
+      <section className="py-5 col-span-3 order-1 xl:order-2 h-full">
         <Card
           className="w-[95%] h-16 mb-4 mx-auto bg-transparent shadow-[0_0_20px_indigo]"
           shadow={false}
@@ -421,7 +421,7 @@ export default function MainSection() {
 
       <section className="grid grid-cols-1 relative order-3 mx-auto w-full h-full">
         <Card
-          className="relative xl:sticky xl:top-0 h-[80%] bg-transparent"
+          className="relative xl:sticky xl:top-0 h-full xl:h-1/2 bg-transparent"
           shadow={false}
         >
           <List className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-1 mx-auto">
