91ce99ff1adb7e8fcf181e38f6118429410dc5e9
Caprice Yuri <123914679+CapriceYuri@users.noreply.github.com>
2024-01-19 10:46:18 -0500

    Re-vision

--- src/MainContent.jsx
+++ src/MainContent.jsx
@@ -1,7 +1,6 @@
 import {
   List,
   ListItem,
-  ListItemPrefix,
   Avatar,
   Card,
   Typography,
@@ -25,6 +24,14 @@ PlayerData.runs.sort((a, b) => (a.time > b.time ? 1 : -1));
 TestData.runs.sort((a, b) => (a.time > b.time ? 1 : -1));
 
 const allData = [PlayerData, TestData];
+const links = [
+  { title: "Perks", src: "https://capriceyuri.github.io/wwzAftermathPerks/" },
+  { title: "Horde", src: "https://capriceyuri.github.io/wwzAftermathHorde/" },
+  {
+    title: "Speedrun",
+    src: "https://capriceyuri.github.io/wwzAftermathExtreme/",
+  },
+];
 // Data Assortment
 
 export default function MainSection() {
@@ -49,59 +56,15 @@ export default function MainSection() {
   // Functions & Callbacks
 
   return (
-    <section className="grid grid-cols-2 xl:grid-cols-5 relative h-lvh overflow-x-hidden">
+    <section className="grid grid-cols-4 relative h-lvh overflow-x-hidden">
       <img
         src="bgtesting.png"
         className="fixed h-lvh w-full object-cover -z-10"
       />
 
-      <section className="grid grid-cols-1 relative order-2 xl:order-1 mx-auto w-full">
+      <section className="py-5 col-span-4 w-[90%] mx-auto">
         <Card
-          className="relative lg:sticky lg:top-0 h-full xl:h-1/2 bg-transparent"
-          shadow={false}
-        >
-          <List className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-1 mx-auto">
-            {HordePlayer.map((dev, index) => (
-              <ListItem
-                key={index}
-                className="hover:bg-transparent cursor-default focus:bg-transparent py-0.5"
-              >
-                <ListItemPrefix className="shrink-0">
-                  <Avatar
-                    variant="circular"
-                    alt="candice"
-                    src={`${dev.img}`}
-                    size="sm"
-                    withBorder={true}
-                    className="p-0.5"
-                    color={dev.id > 10 ? "green" : "deep-orange"}
-                  />
-                </ListItemPrefix>
-
-                <div>
-                  <Typography
-                    variant="small"
-                    color={dev.id > 10 ? "green" : "deep-orange"}
-                    className="font-semibold text-sm"
-                    children={dev.name}
-                  ></Typography>
-
-                  <Typography
-                    variant="small"
-                    color={dev.id > 10 ? "green" : "deep-orange"}
-                    className="font-normal"
-                    children={dev.mark}
-                  ></Typography>
-                </div>
-              </ListItem>
-            ))}
-          </List>
-        </Card>
-      </section>
-
-      <section className="py-5 col-span-3 order-1 xl:order-2">
-        <Card
-          className="w-[95%] h-16 mb-4 mx-auto bg-transparent shadow-[0_0_20px_indigo]"
+          className="w-[90%] h-16 mb-4 mx-auto bg-transparent shadow-[0_0_20px_indigo]"
           shadow={false}
         >
           <img
@@ -109,207 +72,103 @@ export default function MainSection() {
             className="h-full w-full object-cover rounded-2xl absolute"
           />
           <div className="absolute h-full w-full bg-gradient-to-t to-purple-900 from-red-400 opacity-80 rounded-xl" />
-          <nav className="h-full mix">
+          <nav className="h-full mix-blend-overlay">
             <ul className="grid grid-cols-3 h-full justify-evenly place-items-center">
-              <li>
-                <a
-                  href="https://capriceyuri.github.io/wwzAftermathPerks/"
-                  target="_blank"
-                >
-                  <Typography color="orange" variant="h6" className="p1">
-                    Perks
-                  </Typography>
-                </a>
-              </li>
-              <li>
-                <a
-                  href="https://capriceyuri.github.io/wwzAftermathHorde/"
-                  target="_blank"
-                >
-                  <Typography
-                    color="orange"
-                    variant="h6"
-                    className="border-indigo-200"
-                  >
-                    Horde
-                  </Typography>
-                </a>
-              </li>
-              <li>
-                <a
-                  href="https://capriceyuri.github.io/wwzAftermathExtreme/"
-                  target="_blank"
-                >
-                  <Typography color="orange" variant="h6" className="p1">
-                    Speedrun
-                  </Typography>
-                </a>
-              </li>
+              {links.map((link) => (
+                <li>
+                  <a href={link.src} target="_blank">
+                    <Typography color="white" variant="h6">
+                      {link.title}
+                    </Typography>
+                  </a>
+                </li>
+              ))}
             </ul>
           </nav>
         </Card>
-        <Card
-          className="w-[95%] h-56 mx-auto mb-4 bg-transparent shadow-[0_0_20px_red] relative"
-          shadow={false}
-        >
-          <img
-            src="placeholder2.png"
-            className="h-full w-full object-cover rounded-2xl absolute"
-          />
-          <div className="absolute h-full w-full bg-gradient-to-b to-purple-900 from-red-400 opacity-60 rounded-xl" />
-          <div className="grid grid-cols-1 my-auto">
-            <Carousel
-              className="rounded-xl"
-              transition={{ duration: 2 }}
-              loop={true}
-              autoplay={true}
-            >
-              <div className="h-[200px] w-full">
-                <div className="flex flex-row h-full justify-center gap-2 items-center text-start">
-                  <Avatar
-                    src="player-SiberianPsych0.png"
-                    size="xl"
-                    withBorder={true}
-                    className="p-0.5"
-                    color="amber"
-                  />
-                  <div>
-                    <Typography variant="h4" color="amber">
-                      SiberianPsych0
-                    </Typography>
-                    <Typography
-                      variant="h6"
-                      color="cyan"
-                      className="font-semibold"
-                      children={"Descent"}
-                    ></Typography>
-                    <Typography
-                      variant="h6"
-                      color="cyan"
-                      className="font-semibold"
-                      children={"13m 11s"}
-                    ></Typography>
+        <div className="grid grid-cols-1 lg:grid-cols-2 mx-auto w-[90%]">
+          <Card
+            className="w-[90%] h-56 mx-auto mb-4 bg-transparent shadow-[0_0_20px_red] relative"
+            shadow={false}
+          >
+            <img
+              src="placeholder2.png"
+              className="h-full w-full object-cover rounded-2xl absolute"
+            />
+            <div className="absolute h-full w-full bg-gradient-to-b to-purple-900 from-red-400 opacity-60 rounded-xl" />
+
+            <div className="grid grid-cols-1 my-auto">
+              <Carousel className="rounded-xl" loop={true} autoplay={true}>
+                {HordePlayer.map((player) => (
+                  <div className="h-[200px] w-full">
+                    <div className="flex flex-row h-full justify-center gap-2 items-center text-start">
+                      <Avatar
+                        src={player.img}
+                        size="xl"
+                        withBorder={true}
+                        className="p-0.5"
+                        color="amber"
+                      />
+                      <div>
+                        <Typography variant="h4" color="amber">
+                          {player.name}
+                        </Typography>
+                        <Typography
+                          variant="h6"
+                          color="cyan"
+                          className="font-semibold"
+                          children={player.mark}
+                        ></Typography>
+                      </div>
+                    </div>
                   </div>
-                </div>
-              </div>
-              <div className="h-[200px] w-full">
-                <div className="flex flex-row h-full justify-center gap-2 items-center text-start">
-                  <Avatar
-                    src="player-aedonis.png"
-                    size="xl"
-                    withBorder={true}
-                    className="p-0.5"
-                    color="amber"
-                  />
-                  <div>
-                    <Typography variant="h4" color="amber">
-                      Aedonis
-                    </Typography>
-                    <Typography
-                      variant="h6"
-                      color="cyan"
-                      className="font-semibold"
-                      children={"Descent"}
-                    ></Typography>
-                    <Typography
-                      variant="h6"
-                      color="cyan"
-                      className="font-semibold"
-                      children={"100% Headshot Rate"}
-                    ></Typography>
+                ))}
+              </Carousel>
+            </div>
+          </Card>
+          <Card
+            className="w-[90%] h-56 mx-auto mb-4 bg-transparent shadow-[0_0_20px_red] relative"
+            shadow={false}
+          >
+            <img
+              src="placeholder2.png"
+              className="h-full w-full object-cover rounded-2xl absolute"
+            />
+            <div className="absolute h-full w-full bg-gradient-to-b to-purple-900 from-red-400 opacity-60 rounded-xl" />
+
+            <div className="grid grid-cols-1 my-auto">
+              <Carousel className="rounded-xl" loop={true} autoplay={true}>
+                {ExtremePlayer.map((player) => (
+                  <div className="h-[200px] w-full">
+                    <div className="flex flex-row h-full justify-center gap-2 items-center text-start">
+                      <Avatar
+                        src={player.img}
+                        size="xl"
+                        withBorder={true}
+                        className="p-0.5"
+                        color="amber"
+                      />
+                      <div>
+                        <Typography variant="h4" color="amber">
+                          {player.name}
+                        </Typography>
+                        <Typography
+                          variant="h6"
+                          color="cyan"
+                          className="font-semibold"
+                          children={player.mark}
+                        ></Typography>
+                      </div>
+                    </div>
                   </div>
-                </div>
-              </div>
-              <div className="h-[200px] w-full">
-                <div className="flex flex-row h-full justify-center gap-2 items-center text-start">
-                  <Avatar
-                    src="player-Mew.png"
-                    size="xl"
-                    withBorder={true}
-                    className="p-0.5"
-                    color="amber"
-                  />
-                  <div>
-                    <Typography variant="h4" color="amber">
-                      Mewtality
-                    </Typography>
-                    <Typography
-                      variant="h6"
-                      color="cyan"
-                      className="font-semibold"
-                      children={"PVP"}
-                    ></Typography>
-                    <Typography
-                      variant="h6"
-                      color="cyan"
-                      className="font-semibold"
-                      children={"Shadow Abuser"}
-                    ></Typography>
-                  </div>
-                </div>
-              </div>
-              <div className="h-[200px] w-full">
-                <div className="flex flex-row h-full justify-center gap-2 items-center text-start">
-                  <Avatar
-                    src="player-Tina.png"
-                    size="xl"
-                    withBorder={true}
-                    className="p-0.5"
-                    color="amber"
-                  />
-                  <div>
-                    <Typography variant="h4" color="amber">
-                      Tina
-                    </Typography>
-                    <Typography
-                      variant="h6"
-                      color="cyan"
-                      className="font-semibold"
-                      children={"WWZ"}
-                    ></Typography>
-                    <Typography
-                      variant="h6"
-                      color="cyan"
-                      className="font-semibold"
-                      children={"Bug Abuser"}
-                    ></Typography>
-                  </div>
-                </div>
-              </div>
-              <div className="h-[200px] w-full">
-                <div className="flex flex-row h-full justify-center gap-2 items-center text-start">
-                  <Avatar
-                    src="dev-George.png"
-                    size="xl"
-                    withBorder={true}
-                    className="p-0.5"
-                    color="amber"
-                  />
-                  <div>
-                    <Typography variant="h4" color="amber">
-                      George
-                    </Typography>
-                    <Typography
-                      variant="h6"
-                      color="cyan"
-                      className="font-semibold"
-                      children={"Community"}
-                    ></Typography>
-                    <Typography
-                      variant="h6"
-                      color="cyan"
-                      className="font-semibold"
-                      children={"Double EXP & XP"}
-                    ></Typography>
-                  </div>
-                </div>
-              </div>
-            </Carousel>
-          </div>
-        </Card>
+                ))}
+              </Carousel>
+            </div>
+          </Card>
+        </div>
 
         <Card
-          className="w-[95%] mx-auto bg-transparent backdrop-blur-xl shadow-[0_0_35px_purple] relative"
+          className="w-[90%] mx-auto bg-transparent backdrop-blur-xl shadow-[0_0_50px_purple] relative"
           shadow={false}
         >
           <img
@@ -416,50 +275,6 @@ export default function MainSection() {
               </div>
             </div>
           </CardFooter>
-        </Card>
-      </section>
-
-      <section className="grid grid-cols-1 relative order-3 mx-auto w-full">
-        <Card
-          className="relative xl:sticky xl:top-0 h-full xl:h-1/2 bg-transparent"
-          shadow={false}
-        >
-          <List className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-1 mx-auto">
-            {ExtremePlayer.map((dev, index) => (
-              <ListItem
-                key={index}
-                className="hover:bg-transparent cursor-default focus:bg-transparent py-0.5"
-              >
-                <ListItemPrefix className="shrink-0">
-                  <Avatar
-                    variant="circular"
-                    alt="candice"
-                    src={`${dev.img}`}
-                    size="sm"
-                    withBorder={true}
-                    className="p-0.5"
-                    color="light-blue"
-                  />
-                </ListItemPrefix>
-
-                <div>
-                  <Typography
-                    variant="small"
-                    color="blue"
-                    className="font-normal text-sm"
-                    children={dev.name}
-                  ></Typography>
-
-                  <Typography
-                    variant="small"
-                    color="blue"
-                    className="font-normal"
-                    children={dev.mark}
-                  ></Typography>
-                </div>
-              </ListItem>
-            ))}
-          </List>
         </Card>
       </section>
     </section>
