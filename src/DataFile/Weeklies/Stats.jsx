5b49fa10f3f1494b0b0b6525b244793fd12f52f4
Caprice Yuri <123914679+CapriceYuri@users.noreply.github.com>
2024-01-21 11:07:46 -0500

    Added new runner

---
+++ src/DataFile/Weeklies/Stats.jsx
@@ -0,0 +1,15 @@
+export const StatRun = {
+  runs: [
+    {
+      img: "player-aedonis",
+      name: "Aedonis",
+      map: "Descent",
+      diff: "Extreme",
+      src: "https://www.youtube.com/watch?v=Iv-NSDJ2Pow",
+      stat: "100% Headshot",
+      statInfo: "(863/863)",
+      class: "Gunslinger",
+      team: "SOLO",
+    },
+  ],
+};
--- src/MainContent.jsx
+++ src/MainContent.jsx
@@ -19,6 +19,7 @@ import { ExtremePlayer } from "./DataFile/Carousel/ExtremeData";
 import { HordePlayer } from "./DataFile/Carousel/HordeData";
 import { DevStaff } from "./DataFile/Carousel/devData";
 import { ModPlayer } from "./DataFile/Carousel/modData";
+import { StatRun } from "./DataFile/Weeklies/Stats";
 
 // Required Imports
 
@@ -71,7 +72,7 @@ export default function MainSection() {
             src="placeholder3.png"
             className="h-full w-full object-cover rounded-2xl absolute"
           />
-          <div className="absolute h-full w-full bg-gradient-to-t to-purple-900 from-red-300 opacity-80 rounded-xl" />
+          <div className="absolute h-full w-full bg-gradient-to-t from-purple-900 to-red-300 opacity-80 rounded-xl" />
           <nav className="h-full mix-blend-overlay">
             <ul className="grid grid-cols-3 h-full justify-evenly place-items-center">
               {links.map((link) => (
@@ -90,9 +91,114 @@ export default function MainSection() {
             </ul>
           </nav>
         </Card>
+        {/* testing */}
+        <Card
+          className="w-[100%] mx-auto bg-transparent backdrop-blur-xl shadow-[0_0_50px_purple] relative"
+          shadow={false}
+        >
+          <img
+            src="bgtesting.png"
+            className="h-full w-full object-cover rounded-2xl fixed -z-10"
+          />
+          <div className="absolute h-full w-full bg-gradient-to-b from-purple-900 to-red-300 opacity-70 rounded-xl"></div>
+          <List className="z-50">
+            <div>
+              <Typography
+                color="deep-orange"
+                variant="h5"
+                className="text-center p-2"
+                children="Hierarchy Runs"
+              ></Typography>
+            </div>
+            {StatRun.runs.map((player, index) => (
+              <ListItem
+                key={index}
+                className=" hover:bg-black focus:bg-black flex flex-row"
+              >
+                <div className="flex flex-col items-center text-center flex-1">
+                  <Avatar
+                    variant="rounded"
+                    src={`${player.img}.png`}
+                    className="mx-auto xl:mx-1"
+                  />
+                  <div>
+                    <Typography
+                      variant="small"
+                      color="white"
+                      className="font-bold"
+                      children={player.name}
+                    ></Typography>
+                  </div>
+                </div>
+                <div className="mx-4 text-center flex-1">
+                  <Typography
+                    variant="h5"
+                    color="white"
+                    className="font-semibold"
+                    children={player.map}
+                  ></Typography>
+                  <Typography
+                    variant="small"
+                    color="amber"
+                    className="font-normal"
+                    children={player.diff}
+                  ></Typography>
+                </div>
+                <div className="mx-4 text-center flex-1">
+                  <Typography
+                    variant="h5"
+                    color="red"
+                    className="font-semibold"
+                    children={player.team}
+                  ></Typography>
+                  <Typography
+                    variant="small"
+                    color="amber"
+                    className="font-normal"
+                    children={player.class}
+                  ></Typography>
+                </div>
+
+                <div className="mx-4 text-center flex-1">
+                  <Typography
+                    variant="h5"
+                    color="pink"
+                    className="font-semibold"
+                    children={player.stat}
+                  ></Typography>
+
+                  <Typography
+                    variant="h6"
+                    color="amber"
+                    className="font-normal"
+                    children={player.statInfo}
+                  ></Typography>
+                </div>
+
+                <div className="flex justify-center">
+                  <a href={player.src} target="_blank">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                      fill="currentColor"
+                      className="w-6 h-6 text-orange-900 z-10"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M4.5 5.653c0-1.427 1.529-2.33 2.779-1.643l11.54 6.347c1.295.712 1.295 2.573 0 3.286L7.28 19.99c-1.25.687-2.779-.217-2.779-1.643V5.653Z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                  </a>
+                </div>
+              </ListItem>
+            ))}
+          </List>
+        </Card>
+        {/* testing */}
         <div className="grid grid-cols-1 lg:grid-cols-2 mx-auto w-[100%] gap-x-2">
           <Card
-            className="w-[100%] h-56 mx-auto mb-4 bg-transparent shadow-[0_0_40px_purple] relative"
+            className="w-[100%] h-56 mx-auto my-4 bg-transparent shadow-[0_0_40px_purple] relative"
             shadow={false}
           >
             <img
@@ -131,7 +237,7 @@ export default function MainSection() {
             </div>
           </Card>
           <Card
-            className="w-[100%] h-56 mx-auto mb-4 bg-transparent shadow-[0_0_40px_purple] relative"
+            className="w-[100%] h-56 mx-auto my-4 bg-transparent shadow-[0_0_40px_purple] relative"
             shadow={false}
           >
             <img
@@ -259,7 +365,7 @@ export default function MainSection() {
                       xmlns="http://www.w3.org/2000/svg"
                       viewBox="0 0 24 24"
                       fill="currentColor"
-                      className="w-6 h-6 text-green-400 z-10"
+                      className="w-6 h-6 text-orange-900 z-10"
                     >
                       <path
                         fillRule="evenodd"
