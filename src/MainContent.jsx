0bf1ec7ddbe01d71b668a4e3a963beee21924ffd
Caprice Yuri <123914679+CapriceYuri@users.noreply.github.com>
2024-01-22 11:05:06 -0500

    Git Final

--- src/MainContent.jsx
+++ src/MainContent.jsx
@@ -98,194 +98,6 @@ export default function MainSection() {
           shadow={false}
         >
           <img
-            src="bgtesting.png"
-            className="h-full w-full object-cover rounded-2xl fixed -z-10"
-          />
-          <div className="absolute h-full w-full bg-gradient-to-b from-purple-900 to-blue-200 opacity-50 rounded-xl"></div>
-          <List className="z-50">
-            <div>
-              <Typography
-                color="deep-orange"
-                variant="h5"
-                className="text-center p-2"
-                children="HIERARCHY RUNS"
-              ></Typography>
-            </div>
-            {StatRun.runs.map((player, index) => (
-              <ListItem
-                key={index}
-                className=" hover:bg-black focus:bg-black flex flex-row"
-              >
-                <div className="flex flex-col md:text-center md:items-center flex-1">
-                  <Avatar
-                    variant="rounded"
-                    src={`${player.img}.png`}
-                    className="md:mx-auto p-0.5"
-                    withBorder={true}
-                    color="deep-orange"
-                  />
-                  <div>
-                    <Typography
-                      variant="small"
-                      color="white"
-                      className="font-bold"
-                      children={player.name}
-                    ></Typography>
-                  </div>
-                </div>
-                <div className="mx-4 flex-1 text-center">
-                  <Typography
-                    variant="h6"
-                    color="white"
-                    className="font-semibold"
-                    children={player.map}
-                  ></Typography>
-                  <Typography
-                    variant="small"
-                    color="amber"
-                    className="font-normal"
-                    children={player.diff}
-                  ></Typography>
-                </div>
-                <div className="mx-4 flex-1 text-center">
-                  <Typography
-                    variant="h6"
-                    color={player.team === "SOLO" ? "deep-orange" : "cyan"}
-                    className="font-semibold"
-                    children={player.team}
-                  ></Typography>
-                  <Typography
-                    variant="small"
-                    color="amber"
-                    className="font-normal"
-                    children={player.class}
-                  ></Typography>
-                </div>
-
-                <div className="mx-4 text-center flex-1">
-                  <Typography
-                    variant="small"
-                    color="yellow"
-                    className="font-semibold"
-                    children={player.stat}
-                  ></Typography>
-
-                  <Typography
-                    variant="small"
-                    color="amber"
-                    className="font-normal"
-                    children={player.statInfo}
-                  ></Typography>
-                </div>
-
-                <div className="flex justify-center flex-1">
-                  <a href={player.src} target="_blank">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 24 24"
-                      fill="currentColor"
-                      color="yellow"
-                      className="w-6 h-6 z-10"
-                    >
-                      <path
-                        fillRule="evenodd"
-                        d="M4.5 5.653c0-1.427 1.529-2.33 2.779-1.643l11.54 6.347c1.295.712 1.295 2.573 0 3.286L7.28 19.99c-1.25.687-2.779-.217-2.779-1.643V5.653Z"
-                        clipRule="evenodd"
-                      />
-                    </svg>
-                  </a>
-                </div>
-              </ListItem>
-            ))}
-          </List>
-        </Card>
-        {/* testing */}
-        <div className="grid grid-cols-1 lg:grid-cols-2 mx-auto w-[100%] gap-x-2">
-          <Card
-            className="w-[100%] h-56 mx-auto my-4 bg-transparent shadow-[0_0_40px_purple] relative"
-            shadow={false}
-          >
-            <img
-              src="placeholder3.png"
-              className="h-full w-full object-cover rounded-2xl absolute"
-            />
-            <div className="absolute h-full w-full bg-gradient-to-b to-purple-900 from-blue-200 opacity-50 rounded-xl" />
-
-            <div className="grid grid-cols-1 my-auto">
-              <Carousel className="rounded-xl" loop={true} autoplay={true}>
-                {HordePlayer.map((player) => (
-                  <div className="h-[200px] w-full">
-                    <div className="flex flex-row h-full justify-center gap-2 items-center">
-                      <Avatar
-                        src={player.img}
-                        size="xl"
-                        withBorder={true}
-                        className="p-0.5"
-                        color="deep-orange"
-                      />
-                      <div>
-                        <Typography variant="h4" color="white">
-                          {player.name}
-                        </Typography>
-                        <Typography
-                          variant="h6"
-                          color="deep-orange"
-                          className="font-semibold"
-                          children={player.mark}
-                        ></Typography>
-                      </div>
-                    </div>
-                  </div>
-                ))}
-              </Carousel>
-            </div>
-          </Card>
-          <Card
-            className="w-[100%] h-56 mx-auto my-4 bg-transparent shadow-[0_0_40px_purple] relative"
-            shadow={false}
-          >
-            <img
-              src="placeholder2.png"
-              className="h-full w-full object-cover rounded-2xl absolute"
-            />
-            <div className="absolute h-full w-full bg-gradient-to-b to-purple-900 from-blue-200 opacity-50 rounded-xl" />
-
-            <div className="grid grid-cols-1 my-auto">
-              <Carousel className="rounded-xl" loop={true} autoplay={true}>
-                {ExtremePlayer.map((player) => (
-                  <div className="h-[200px] w-full">
-                    <div className="flex flex-row h-full justify-center gap-2 items-center text-start">
-                      <Avatar
-                        src={player.img}
-                        size="xl"
-                        withBorder={true}
-                        className="p-0.5"
-                        color="blue"
-                      />
-                      <div>
-                        <Typography variant="h4" color="white">
-                          {player.name}
-                        </Typography>
-                        <Typography
-                          variant="h6"
-                          color="light-blue"
-                          className="font-semibold"
-                          children={player.mark}
-                        ></Typography>
-                      </div>
-                    </div>
-                  </div>
-                ))}
-              </Carousel>
-            </div>
-          </Card>
-        </div>
-
-        <Card
-          className="w-[100%] mx-auto bg-transparent backdrop-blur-xl shadow-[0_0_50px_purple] relative"
-          shadow={false}
-        >
-          <img
             src="placeholder.png"
             className="h-full w-full object-cover rounded-2xl fixed -z-10"
           />
@@ -393,6 +205,194 @@ export default function MainSection() {
               </div>
             </div>
           </CardFooter>
+        </Card>
+        {/* testing */}
+        <div className="grid grid-cols-1 lg:grid-cols-2 mx-auto w-[100%] gap-x-2">
+          <Card
+            className="w-[100%] h-56 mx-auto my-4 bg-transparent shadow-[0_0_40px_purple] relative"
+            shadow={false}
+          >
+            <img
+              src="placeholder3.png"
+              className="h-full w-full object-cover rounded-2xl absolute"
+            />
+            <div className="absolute h-full w-full bg-gradient-to-b to-purple-900 from-blue-200 opacity-50 rounded-xl" />
+
+            <div className="grid grid-cols-1 my-auto">
+              <Carousel className="rounded-xl" loop={true} autoplay={true}>
+                {HordePlayer.map((player) => (
+                  <div className="h-[200px] w-full">
+                    <div className="flex flex-row h-full justify-center gap-2 items-center">
+                      <Avatar
+                        src={player.img}
+                        size="xl"
+                        withBorder={true}
+                        className="p-0.5"
+                        color="deep-orange"
+                      />
+                      <div>
+                        <Typography variant="h4" color="white">
+                          {player.name}
+                        </Typography>
+                        <Typography
+                          variant="h6"
+                          color="deep-orange"
+                          className="font-semibold"
+                          children={player.mark}
+                        ></Typography>
+                      </div>
+                    </div>
+                  </div>
+                ))}
+              </Carousel>
+            </div>
+          </Card>
+          <Card
+            className="w-[100%] h-56 mx-auto my-4 bg-transparent shadow-[0_0_40px_purple] relative"
+            shadow={false}
+          >
+            <img
+              src="placeholder2.png"
+              className="h-full w-full object-cover rounded-2xl absolute"
+            />
+            <div className="absolute h-full w-full bg-gradient-to-b to-purple-900 from-blue-200 opacity-50 rounded-xl" />
+
+            <div className="grid grid-cols-1 my-auto">
+              <Carousel className="rounded-xl" loop={true} autoplay={true}>
+                {ExtremePlayer.map((player) => (
+                  <div className="h-[200px] w-full">
+                    <div className="flex flex-row h-full justify-center gap-2 items-center text-start">
+                      <Avatar
+                        src={player.img}
+                        size="xl"
+                        withBorder={true}
+                        className="p-0.5"
+                        color="blue"
+                      />
+                      <div>
+                        <Typography variant="h4" color="white">
+                          {player.name}
+                        </Typography>
+                        <Typography
+                          variant="h6"
+                          color="light-blue"
+                          className="font-semibold"
+                          children={player.mark}
+                        ></Typography>
+                      </div>
+                    </div>
+                  </div>
+                ))}
+              </Carousel>
+            </div>
+          </Card>
+        </div>
+
+        <Card
+          className="w-[100%] mx-auto bg-transparent backdrop-blur-xl shadow-[0_0_50px_purple] relative"
+          shadow={false}
+        >
+          <img
+            src="bgtesting.png"
+            className="h-full w-full object-cover rounded-2xl fixed -z-10"
+          />
+          <div className="absolute h-full w-full bg-gradient-to-b from-purple-900 to-blue-200 opacity-50 rounded-xl"></div>
+          <List className="z-50">
+            <div>
+              <Typography
+                color="deep-orange"
+                variant="h5"
+                className="text-center p-2"
+                children="HIERARCHY RUNS"
+              ></Typography>
+            </div>
+            {StatRun.runs.map((player, index) => (
+              <ListItem
+                key={index}
+                className=" hover:bg-black focus:bg-black flex flex-row"
+              >
+                <div className="flex flex-col md:text-center md:items-center flex-1">
+                  <Avatar
+                    variant="rounded"
+                    src={`${player.img}.png`}
+                    className="md:mx-auto p-0.5"
+                    withBorder={true}
+                    color={player.placeholder == true ? "yellow" : "blue"}
+                  />
+                  <div>
+                    <Typography
+                      variant="small"
+                      color="white"
+                      className="font-bold"
+                      children={player.name}
+                    ></Typography>
+                  </div>
+                </div>
+                <div className="mx-4 flex-1 text-center">
+                  <Typography
+                    variant="h6"
+                    color="white"
+                    className="font-semibold"
+                    children={player.map}
+                  ></Typography>
+                  <Typography
+                    variant="small"
+                    color="amber"
+                    className="font-normal"
+                    children={player.diff}
+                  ></Typography>
+                </div>
+                <div className="mx-4 flex-1 text-center">
+                  <Typography
+                    variant="h6"
+                    color={player.team === "SOLO" ? "deep-orange" : "cyan"}
+                    className="font-semibold"
+                    children={player.team}
+                  ></Typography>
+                  <Typography
+                    variant="small"
+                    color="amber"
+                    className="font-normal"
+                    children={player.class}
+                  ></Typography>
+                </div>
+
+                <div className="mx-4 text-center flex-1">
+                  <Typography
+                    variant="small"
+                    color="yellow"
+                    className="font-semibold"
+                    children={player.stat}
+                  ></Typography>
+
+                  <Typography
+                    variant="small"
+                    color="amber"
+                    className="font-normal"
+                    children={player.statInfo}
+                  ></Typography>
+                </div>
+
+                <div className="flex justify-center flex-1">
+                  <a href={player.src} target="_blank">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                      fill="currentColor"
+                      color="yellow"
+                      className="w-6 h-6 z-10"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M4.5 5.653c0-1.427 1.529-2.33 2.779-1.643l11.54 6.347c1.295.712 1.295 2.573 0 3.286L7.28 19.99c-1.25.687-2.779-.217-2.779-1.643V5.653Z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                  </a>
+                </div>
+              </ListItem>
+            ))}
+          </List>
         </Card>
         <div className="grid grid-cols-1 lg:grid-cols-2 mx-auto w-[100%] gap-x-2">
           <Card
