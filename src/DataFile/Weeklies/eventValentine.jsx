8c226ee16d4b55c616f91583e356caab54cec8ec
Caprice Yuri <123914679+CapriceYuri@users.noreply.github.com>
2024-02-14 19:29:39 -0500

    Create Duo Event

---
+++ src/DataFile/Weeklies/eventValentine.jsx
@@ -0,0 +1,15 @@
+export const eventValentine = {
+  title: "❤️ Bloody Valentine Event ❤️",
+  runs: [
+    {
+      duoname: "Tina & Mew",
+      p1_img: "player-Tina",
+      p2_img: "player-Mew",
+      map: "Marseille",
+      diff: "Normal",
+      src: "",
+      time: 1200,
+      team: "DUO",
+    },
+  ],
+};
--- src/MainContent.jsx
+++ src/MainContent.jsx
@@ -17,6 +17,7 @@ import ExtremeCarousel from "./DataFile/Compo/SpeedrunCarousel";
 import TotalRunsCard from "./DataFile/Compo/TotalRunCard";
 import TotalPlayerCard from "./DataFile/Compo/TotalPlayerCard";
 import NavigationBar from "./DataFile/Compo/Nav";
+import { eventValentine } from "./DataFile/Weeklies/eventValentine";
 
 import { allData } from "./DataFile/Weeklies/DataLogic";
 
@@ -53,7 +54,120 @@ export default function MainSection() {
           <TotalRunsCard />
           <TotalPlayerCard />
         </div>
+        {/* TEST */}
+        <Card
+          className="w-[100%] mx-auto mb-4 bg-transparent rounded-2xl backdrop-blur-xl shadow-[0_0_20px_red] relative"
+          shadow={false}
+        >
+          <div className="bg-gradient-to-t from-pink-600 to-purple-800 h-full w-full object-cover rounded-2xl fixed -z-10 opacity-60" />
+          <List className="z-50">
+            <div>
+              <Typography
+                color="red"
+                variant="h4"
+                className="text-center p-2 rounded-2xl"
+                children={eventValentine.title}
+              ></Typography>
+            </div>
+            {eventValentine.runs.map((player, index) => (
+              <ListItem
+                key={index}
+                className=" hover:bg-black focus:bg-black flex flex-row relative"
+              >
+                <div
+                  className={`bg-[url(/rings/valentinebg.gif)] border-2 border-red-400 rounded-2xl bg-contain opacity-70 absolute h-full w-full top-0 left-0 -z-10 bg-black`}
+                />
+                <div className="flex flex-col flex-1 text-center">
+                  <div>
+                    <div className="flex justify-center">
+                      <div className="relative">
+                        <Avatar
+                          size="lg"
+                          variant="circular"
+                          src={`${player.p1_img}.png`}
+                          withBorder={true}
+                          className="p-0.05"
+                        />
+                        <img
+                          src={`rings/discord-pink.gif`}
+                          className="absolute top-0 start-0"
+                        />
+                      </div>
+                      <div className="relative">
+                        <Avatar
+                          size="lg"
+                          variant="circular"
+                          src={`${player.p2_img}.png`}
+                          withBorder={true}
+                          className="p-0.5"
+                        />
+                        <img
+                          src={`rings/discord-pink.gif`}
+                          className="absolute top-0 start-0"
+                        />
+                      </div>
+                    </div>
 
+                    <div>
+                      <Typography
+                        variant="small"
+                        color="white"
+                        className="font-bold"
+                        children={player.duoname}
+                      ></Typography>
+                    </div>
+                  </div>
+                </div>
+                <div className="flex-1 text-center hidden md:block">
+                  <Typography
+                    variant="h6"
+                    color="yellow"
+                    className="font-semibold"
+                    children={player.map}
+                  ></Typography>
+                  <Typography
+                    variant="small"
+                    color="white"
+                    className="font-normal"
+                    children={player.diff}
+                  ></Typography>
+                </div>
+                <div className="flex-1 text-center">
+                  <Typography
+                    variant="h5"
+                    color="cyan"
+                    className="font-semibold"
+                    children={player.team}
+                  ></Typography>
+                  <Typography
+                    variant="h6"
+                    color="yellow"
+                    className="font-semibold"
+                    children={conversion(player.time)}
+                  ></Typography>
+                </div>
+                <div className="flex justify-center">
+                  <a href={player.src} target="_blank">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                      fill="currentColor"
+                      color="yellow"
+                      className="w-6 h-6 z-10 animate-bounce"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M4.5 5.653c0-1.427 1.529-2.33 2.779-1.643l11.54 6.347c1.295.712 1.295 2.573 0 3.286L7.28 19.99c-1.25.687-2.779-.217-2.779-1.643V5.653Z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                  </a>
+                </div>
+              </ListItem>
+            ))}
+          </List>
+        </Card>
+        {/* TEST */}
         <Card
           className="w-[100%] mx-auto bg-transparent backdrop-blur-xl shadow-[0_0_50px_purple] relative"
           shadow={false}
@@ -67,8 +181,8 @@ export default function MainSection() {
             <div>
               <Typography
                 color="deep-orange"
-                variant="h5"
-                className="text-center p-2"
+                variant="h4"
+                className="text-center p-2 font-semibold"
                 children="HIERARCHY RUNS"
               ></Typography>
             </div>
@@ -151,8 +265,8 @@ export default function MainSection() {
                       xmlns="http://www.w3.org/2000/svg"
                       viewBox="0 0 24 24"
                       fill="currentColor"
-                      color="yellow"
-                      className="w-6 h-6 z-10"
+                      color="red"
+                      className="w-6 h-6 z-10 animate-bounce"
                     >
                       <path
                         fillRule="evenodd"
@@ -296,8 +410,8 @@ export default function MainSection() {
                       xmlns="http://www.w3.org/2000/svg"
                       viewBox="0 0 24 24"
                       fill="currentColor"
-                      color="yellow"
-                      className="w-6 h-6 z-10"
+                      color="red"
+                      className="w-6 h-6 z-10 animate-bounce"
                     >
                       <path
                         fillRule="evenodd"
